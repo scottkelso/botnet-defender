dddbc257445aa602a5e77ae194dcf9cb0be585a5
40129940 <jkelso07@qub.ac.uk>
2018-12-11 16:55:43 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # botnet-defender
 A proposed botnet detector tailored towards Mirai-like botnets which uses machine learning to detect the preliminary stages and reacts to the threat in an SDN environment.
+
+# Prerequisits
+* [Docker](https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce-1)
+* [Faucet, Grafana, Prometheus](https://faucet.readthedocs.io/en/latest/tutorials/first_time.html)
+
+# Setup Guide
+* [Docker](https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce-1)
+* [Add sudo privileges](https://docs.docker.com/install/linux/linux-postinstall/#manage-docker-as-a-non-root-user) to docker
+* Install [Faucet, Grafana, Prometheus & Guage](https://faucet.readthedocs.io/en/latest/tutorials/first_time.html)
+
+# Related Work
+https://github.com/scottkelso/sdn-security
+https://github.com/a-blaise/split-and-merge
+https://github.com/CyberReboot/PoseidonML
+
+# Data
+http://mawi.wide.ad.jp/mawi/samplepoint-F/2016/201609201400.html
---
+++ run-faucnet.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# PREREQUISITS...
+# ~/Documents/csc4006/docker-mininet
+# ~/Documents/csc4006/traffic
+# docker & docker group
+# (https://docs.docker.com/install/linux/linux-postinstall/#manage-docker-as-a-non-root-user)
+# sudo xhost
+# assuming that faucet directories have been setup
+
+# Grab local IP address
+ip=`hostname -I | awk '{print $1}'`
+echo "Using $ip as host IP address"
+echo "Ensure this is correct to allow mininet to conect to the faucet and gauge controllers"
+echo "Can be omitted for machines with static ip address\n"
+
+# Add permissions for mininet's xterms on the host machine
+sudo xhost +
+
+# Run Mininet
+cd ~/Documents/csc4006/docker-mininet
+docker build -t mininet .
+docker run -it --rm --privileged \
+            -e DISPLAY -e DOCKER_HOST=$ip \
+            -v /tmp/.X11-unix:/tmp/.X11-unix \
+            -v /lib/modules:/lib/modules \
+            -v ~/Documents/csc4006/traffic:/traffic/ mininet
+
+# Run mininet topology
+# mn --topo single,10 --mac --controller=remote,ip=$DOCKER_HOST,port=6653 --controller=remote,ip=$DOCKER_HOST,port=6654 --switch ovsk
