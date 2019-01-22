19fd241b45ac6837731b53bee8af5016631b59bc
40129940 <jkelso07@qub.ac.uk>
2019-01-22 10:49:02 +0000

    Add argus to setup folder

--- setup/argus.sh
+++ setup/argus.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-##### POSTREQUISITS #####
-
 # Run mininet topology
 mn --topo single,10 --mac --controller=remote,ip=$DOCKER_HOST,port=6653 --controller=remote,ip=$DOCKER_HOST,port=6654 --switch ovsk
-
 
 # Capture traffic on mirror port 10
 timeout 5s tcpdump -w /traffic/capture/file.pcap -i h10-eth0
