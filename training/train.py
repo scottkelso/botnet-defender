b1486027274eaf1e362e84cde7d2e0b5ceaab547
40129940 <jkelso07@qub.ac.uk>
2019-01-23 15:26:10 +0000

    Preprocess OS_Scan and Service_Scan however training fails with

--- training/train.py
+++ training/train.py
@@ -1,6 +1,7 @@
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
+from sklearn.utils import resample
 from sklearn.preprocessing import OrdinalEncoder
 from joblib import dump
 
@@ -24,10 +25,17 @@ def int2ip(addr):
     return socket.inet_ntoa(struct.pack("!I", addr))
 
 
+# df['saddr'] = df['saddr'].apply(ip2int)
+# df['daddr'] = df['daddr'].apply(ip2int)
+# df['srcid'] = df['srcid'].apply(ip2int)
+
+
 def load_os_csv():
-    print("Reading CSV...")
-    all_data = pd.read_csv('data/OS_Scan.csv', sep=';')
-    df = all_data.drop(
+    print("Reading OS_Scan CSV...")
+    df = pd.read_csv('../data/OS_Scan.csv', sep=';')
+
+    # Remove unneeded columns
+    df = df.drop(
         ['subcategory',
          'attack',
          'record',
@@ -40,35 +48,52 @@ def load_os_csv():
          'seq'], axis=1
     )
 
-    print("Preprocessing Data...")
+    # TODO(jk) Loop
+    print("Dropping rows with wrong data...")
     # Drop all ipv6 and icmp packets
-    # df = df[~df.proto.str.contains("ipv6")]
-    # df = df[~df.proto.str.contains("icmp")]
-    # df = df.reset_index(drop=True)
+    df = df[~df.proto.str.contains("ipv6", na=False)]
+    df = df[~df.proto.str.contains("icmp", na=False)]
 
-    # # TODO(jk): Make work for none tcp packets
-    # Select only tcp packets
-    df = df[df['proto'].str.contains("tcp")]
-    df = df.drop(['proto'], axis=1)
+    # Reset indexes that now have missing values
     df = df.reset_index(drop=True)
 
-    # Convert ip addresses to int
-    ip_address_labels = ['saddr', 'daddr', 'srcid']
-    # TODO(jk): For loop
-    df['saddr'] = df['saddr'].apply(ip2int)
-    df['daddr'] = df['daddr'].apply(ip2int)
-    df['srcid'] = df['srcid'].apply(ip2int)
+    # Convert NA values to port -1
+    df.sport = pd.to_numeric(df.sport.fillna(-1))
+    df.dport = pd.to_numeric(df.dport.fillna(-1))
 
-    # df.proto = df.proto.astype('category').cat.codes
-    df.dir = df.dir.astype('category').cat.codes
-    df.state = df.state.astype('category').cat.codes
+    # df = downsample(df)
 
-    return df
+    data = df.drop(columns=['category'])
+    labels = df['category']
+
+    # Numerate categorical data
+    print("Transforming categorical data to numeric...")
+    # TODO(jk): Refactor obj
+    enc = OrdinalEncoder()
+    data = enc.fit_transform(data, y=labels)
+
+    print("Finished loading CSV.\n")
+
+    return data, labels
 
 
 def load_service_csv():
-    print("Reading CSV...")
+    print("Reading Service_Scan CSV...")
     df = pd.read_csv('../data/Service_Scan.csv', sep=';', dtype={'sport': np.object, 'dport': np.object})
+
+    # Remove unneeded columns
+    df = df.drop(
+        ['subcategory',
+         'attack',
+         'record',
+         'dco',
+         'sco',
+         'doui',
+         'soui',
+         'dmac',
+         'smac',
+         'seq'], axis=1
+    )
 
     # TODO(jk) Loop
     print("Dropping rows with wrong data...")
@@ -89,57 +114,59 @@ def load_service_csv():
     df.sport = pd.to_numeric(df.sport.fillna(-1))
     df.dport = pd.to_numeric(df.dport.fillna(-1))
 
-    # Remove unneeded columns
-    df = df.drop(
-        ['subcategory',
-         'attack',
-         'record',
-         'dco',
-         'sco',
-         'doui',
-         'soui',
-         'dmac',
-         'smac',
-         'seq'], axis=1
-    )
+    # df = downsample(df)
 
-    x = df.drop(columns=['category'])
-    y = df['category']
-    print(df['category'].value_counts())
+    data = df.drop(columns=['category'])
+    labels = df['category']
 
-    # df.proto = enc.fit(df.proto)
-    # df.saddr = enc.fit(df.saddr)
-    # df.daddr = enc.fit(df.daddr)
-    # df.dir = enc.fit(df.dir)
-    # df.state = enc.fit(df.state)
-    # df.srcid = enc.fit(df.srcid)
-
-    # TODO(jk): Refactor obj
     # Numerate categorical data
     print("Transforming categorical data to numeric...")
+    # TODO(jk): Refactor obj
     enc = OrdinalEncoder()
-    x = enc.fit_transform(x, y=y)
+    data = enc.fit_transform(data, y=labels)
 
-    return x, y
+    print("Finished loading CSV.\n")
+
+    return data, labels
 
 
-# data = read_os_csv()
-# data = preprocessing(data)
+def downsample(df):
+    print("Before resampling...")
+    print(df.category.value_counts(), "\n")
 
-X, y = load_service_csv()
+    # Separate majority and minority classes
+    df_majority = df[df.category == "Reconnaissance"]
+    df_minority = df[df.category == "Normal"]
 
-# X = data.drop(columns=['category'])
-# y = data['category']
-# print(data['category'].value_counts())
-#
-#
-# # Split the data into training, validation, and testing sets
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-#
-# print("Training SVC Model...")
-# clf = svm.SVC(gamma='scale', probability=True, max_iter=1000)
-# clf.fit(X_train, y_train)
-#
+    # Downsample majority class
+    df_majority_downsampled = resample(df_majority,
+                                       replace=False,  # sample without replacement
+                                       n_samples=len(df_minority),  # to match minority class
+                                       random_state=123)  # reproducible results
+
+    # Combine minority class with downsampled majority class
+    df_downsampled = pd.concat([df_majority_downsampled, df_minority])
+
+    # Display new class counts
+    print("After resampling...")
+    print(df_downsampled.category.value_counts(), "\n")
+
+    return df_downsampled
+
+
+Xos, yos = load_os_csv()
+Xser, yser = load_service_csv()
+
+X = np.concatenate((Xos, Xser), axis=0)
+y = np.concatenate((yos, yser), axis=0)
+
+# Split the data into training, validation, and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+print("Training SVC Model...")
+clf = svm.SVC(gamma='scale', probability=True, max_iter=1000)
+clf.fit(X_train, y_train)
+
 # # # Evaulate the model on the augmented test data
 # # means = X_train.mean(axis=0)
 # # stds = X_train.std(axis=0)
