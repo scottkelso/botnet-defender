8498bd9c0c5d0eced70fc7a5291399f447f3155b
40129940 <jkelso07@qub.ac.uk>
2019-02-01 14:29:47 +0000

    Adding K Fold Cross Validation to SVC

--- training/trainSVM.py
+++ training/trainSVM.py
@@ -1,5 +1,5 @@
 from sklearn import svm
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import f1_score, accuracy_score
 from joblib import dump
 from training.dataLoader import get_data
@@ -11,18 +11,22 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 print("Training SVC Model...")
 clf = svm.SVC(gamma='scale', probability=True, max_iter=1000)
-clf.fit(X_train, y_train)
 
-# # Evaulate the model on the augmented test data
-# means = X_train.mean(axis=0)
-# stds = X_train.std(axis=0)
+scores = cross_val_score(clf, X, y, cv=5)
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+# clf.fit(X_train, y_train)
 #
-# X_test_input = X_test - np.expand_dims(means, 0)
-# X_test_input /= np.expand_dims(stds, 0)
-
-predictions = clf.predict(X_test)
-# print("F1 score:", f1_score(X_test, predictions, average='weighted'))
-
-print("Accuracy:", accuracy_score(y_test, predictions))
+# # # Evaulate the model on the augmented test data
+# # means = X_train.mean(axis=0)
+# # stds = X_train.std(axis=0)
+# #
+# # X_test_input = X_test - np.expand_dims(means, 0)
+# # X_test_input /= np.expand_dims(stds, 0)
+#
+# predictions = clf.predict(X_test)
+# # print("F1 score:", f1_score(X_test, predictions, average='weighted'))
+#
+# print("Accuracy:", accuracy_score(y_test, predictions))
 
 dump(clf, 'svm.joblib')
